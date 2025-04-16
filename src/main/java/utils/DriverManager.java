728e5f7201e3f573f00c021f5ddda5baa2691c34
laxmi kant joshi <lkjoshi.tech@gmail.com>
2025-04-16 18:01:07 +0100

    updated pom.xml with slf4j

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -2,8 +2,11 @@ package utils;
 
 import dataProvider.ConfigFileReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.logging.Log;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static utils.StringExtract.getString;
 
@@ -11,6 +14,7 @@ import static utils.StringExtract.getString;
 public class DriverManager {
     private static WebDriver driver;
     private final ConfigFileReader configFileReader = new ConfigFileReader();
+    private static final Logger logger = LoggerFactory.getLogger(DriverManager.class);
 
     public WebDriver getDriver() {
         if (driver == null) {
@@ -24,6 +28,7 @@ public class DriverManager {
     }
 
     private void initializeRemoteDriver() {
+    logger.info("*********TESTS BEING RUN REMOTELY*********");
         try {
             driver = LambdaTest.setCapability();
             driver.get(configFileReader.getApplicationUrl());
@@ -33,13 +38,14 @@ public class DriverManager {
     }
 
     private void initializeLocalDriver() {
+        logger.info("*********TESTS BEING RUN LOCALLY*********");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(getString("homePage.url"));
     }
 
-    public  void quitDriver() {
+    public void quitDriver() {
         if (driver != null) {
             driver.quit();
             driver = null;
